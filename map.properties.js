b3db54933be84e5cc9037418ad88384c990b1831
ErikPDev <33035080+ErikPDev@users.noreply.github.com>
2022-04-22 03:03:45 +0300

    Initial commit

---
+++ map.properties.js
@@ -0,0 +1,13 @@
+var MapProperties = {
+    minZoom: -6,
+    maxZoom: 0,
+    defaultZoom: 0,
+    imageFormat: "jpeg",
+    minRegionX: -4,
+    minRegionZ: -4,
+    maxRegionX: 3,
+    maxRegionZ: 3,
+    worldName: "Map 4k",
+    background: "",
+    markers: []
+}
